6a32c124146f7d65991b9bfe8017cb6359188b0b
solodezaldivar <ramon.solodezaldivar@uzh.ch>
2023-04-21 16:13:05 +0200

    Implement toggle button

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleButton : MonoBehaviour
+{
+
+    public GameObject toggleButton1;
+    public GameObject toggleButton2;
+    public bool defaultToggle;
+    private bool isToggled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleButton2.SetActive(false);
+        toggleButton1.SetActive(true);
+        if (defaultToggle)
+        {
+            ToggleToggleButton();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleToggleButton()
+    {
+        isToggled = !isToggled;
+        toggleButton2.SetActive(!isToggled);
+        toggleButton1.SetActive(isToggled);
+
+    }
+}
